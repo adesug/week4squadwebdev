684352da12e23528755276bf2a1b2aa07fb669f5
adesug <adesugiantoro89@gmail.com>
2021-04-26 01:51:16 +0700

    PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config({});
 
 const express = require ('express');
 const app = express()
-const port = process.env.PORT || 3001 
+// const port = process.env.PORT || 3001 
 
 
 const mainRoutes = require ('./src/routes')
@@ -10,6 +10,9 @@ const mainRoutes = require ('./src/routes')
 app.use(express.urlencoded ({extended :false}));
 app.use('/', mainRoutes)
 
-app.listen( port, () => {
-    console.log("port jalan pada " + port);
-})
+// app.listen( port, () => {
+//     console.log("port jalan pada " + port);
+// })
+app.listen(process.env.PORT || 3001, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
